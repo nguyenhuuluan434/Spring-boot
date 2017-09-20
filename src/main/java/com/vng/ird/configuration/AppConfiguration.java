a567340061a402dc066b0f93d2b1ea63581cec7b
Nguyen Huu Luan <nguyenhuuluan434@gmail.com>
2017-09-20 16:27:12 +0700

    add new files

--- src/main/java/com/vng/ird/configuration/AppConfiguration.java
+++ src/main/java/com/vng/ird/configuration/AppConfiguration.java
@@ -1,11 +1,24 @@
 package com.vng.ird.configuration;
 
+import java.io.File;
+import java.net.URL;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.UrlResource;
 
 @Configuration
 @EnableAutoConfiguration
 public class AppConfiguration {
 
-	
+	@Bean
+	public PropertyPlaceholderConfigurer propertyConfigurer() throws Exception {
+		PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+		URL url = new File("config/app.properties").toURI().toURL();
+		UrlResource resource = new UrlResource(url);
+		ppc.setLocations(resource);
+		return ppc;
+	}
 }
