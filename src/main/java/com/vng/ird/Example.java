ef13852718c4c21c51eef8ada2f1cb5ab9c31daf
Nguyen Huu Luan <nguyenhuuluan434@gmail.com>
2017-09-20 14:59:20 +0700

    add new file

--- src/main/java/com/vng/ird/Example.java
+++ src/main/java/com/vng/ird/Example.java
@@ -1,12 +1,17 @@
 package com.vng.ird;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
+/*
+ * @EnableAutoConfiguration
+ * 
+ * @ComponentScan
+ */
+@SpringBootApplication
 public class Example {
 	@RequestMapping("/")
 	String home() {
---
+++ src/main/java/com/vng/ird/classloader/ClassLoaderExtend.java
@@ -0,0 +1,12 @@
+package com.vng.ird.classloader;
+
+public class ClassLoaderExtend extends ClassLoader {
+
+	@Override
+	protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+		// TODO Auto-generated method stub
+		
+		return super.loadClass(name, resolve);
+	}
+
+}
